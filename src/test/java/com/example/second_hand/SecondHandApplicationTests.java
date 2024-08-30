97b98fdbeef27743fb48dd870113c06aa10262bd
Thien Truong <caothien231@gmail.com>
2024-08-30 15:50:32 -0700

    initialize project

---
+++ src/test/java/com/example/second_hand/SecondHandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.second_hand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
